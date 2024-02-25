f88a18cb92fc2e4b717c0f2ac84841a8c78e4cb8
Ahmad6556 <wageh4568@gmail.com>
2024-02-25 09:32:22 +0300

    f

--- app.js
+++ app.js
@@ -153,15 +153,3 @@ app.get("/sfuv", (req, res) => {
 app.get("/support", (req, res) => {
   res.render("support")
 })
-
-// This file was automatically added by edgio init.
-// You should commit this file to source control.
-import { Router } from '@edgio/core'
-
-export default new Router()
-  .match('/:path*', {
-    origin: {
-      set_origin: 'edgio_serverless',
-    },
-  })
-  .static('public')
